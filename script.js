f06e91c65bfa789e1b68282f018138c5d5b9bd7e
Patrick Legislador <patlegislador@gmail.com>
2019-08-15 21:25:25 -0400

    Additional changes to animation and content

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ const genBtn = document.querySelector('.generator');
 const textArea = document.querySelector('.text-area');
 const exitBtn = document.querySelector('.exit');
 
-const category = document.querySelector('.category');
-const choices = document.querySelector('.category-shown');
+const displayBox = document.querySelector('.display-box');
+const displayText = document.querySelector('.cat-title');
+const choices = document.querySelector('.category');
 
 genBtn.addEventListener('click', btn => {
     var jokesNum = Math.floor(Math.random() * jokes.length);
@@ -14,12 +15,12 @@ genBtn.addEventListener('click', btn => {
     } else textArea.textContent = jokes[jokesNum];
 });
 
-
 exitBtn.addEventListener('click', btn => {
-    category.classList.add('animated', 'zoomOut');
+    displayBox.classList.add('animated', 'zoomOut');
 });
 
 choices.addEventListener('click', btn => {
-    category.classList.remove('zoomOut');
-    category.classList.add('zoomIn');
+    displayBox.classList.remove('zoomOut');
+    displayBox.classList.add('zoomIn');
+    displayText.textContent = choices.textContent;
 });
