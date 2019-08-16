248dcf5289451928969d83767194848625532c7b
Patrick Legislador <patlegislador@gmail.com>
2019-08-15 20:38:41 -0400

    Added Browsersync for auto refresh. Added basic animations

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const genBtn = document.querySelector('.generator');
 const textArea = document.querySelector('.text-area');
+const exitBtn = document.querySelector('.exit');
+
+const category = document.querySelector('.category');
+const choices = document.querySelector('.category-shown');
 
 genBtn.addEventListener('click', btn => {
     var jokesNum = Math.floor(Math.random() * jokes.length);
@@ -8,4 +12,14 @@ genBtn.addEventListener('click', btn => {
         jokesNum = Math.floor(Math.random() * jokes.length);
         textArea.textContent = jokes[jokesNum];
     } else textArea.textContent = jokes[jokesNum];
+});
+
+
+exitBtn.addEventListener('click', btn => {
+    category.classList.add('animated', 'zoomOut');
+});
+
+choices.addEventListener('click', btn => {
+    category.classList.remove('zoomOut');
+    category.classList.add('zoomIn');
 });
