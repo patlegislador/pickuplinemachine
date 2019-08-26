cdb86f304349915b914cafcaad8f65af70c82965
Patrick Legislador <patlegislador@gmail.com>
2019-08-26 13:11:03 -0400

    Version 2 | Changes to code structure

--- data.js
+++ data.js
@@ -1,50 +1,61 @@
-
-const cheesies = [
-"Are you French because Eiffel for you.",
-"I must be a snowflake, because I've fallen for you.",
-"Hello, I'm a thief, and I'm here to steal your heart.",
-"Are you cake? Cause I want a piece of that.",
-"If nothing lasts forever, will you be my nothing?",
-"Are you from Tennessee? Because you're the only 10 I see!",
-"What has 36 teeth and holds back the Incredible Hulk? My zipper.",
-"Are you lost ma'am? Because heaven is a long way from here.",
-"If you were a steak you would be well done.",
-"Life without you is like a broken pencil... pointless."
-];
-
-
-const dirties = [
-    "Even if there wasn't gravity on earth, I'd still fall for you.",
-    "Roses are red, violets are blue, how would you like it if I came home with you?",
-    "I wish I were cross-eyed so I can see you twice",
-    "Your lips look so lonely…Would they like to meet mine?",
-    "Are you a parking ticket? ‘Cause you’ve got fine written all over you.",
-    "Thank god I'm wearing gloves because you are too hot to handle.",
-    "If a fat man puts you in a bag at night, don't worry I told Santa I wanted you for Christmas.",
-    "I'm no photographer, but I can picture us together.",
-    "Do your legs hurt from running through my dreams all night?",
-    "Pinch me, you’re so fine I must be dreaming.",
-    "Are you a 90 degree angle? Cause you are looking right!",
-    "Are you Israeli? Cause you Israeli hot.",
-    "On a scale from 1 to 10, you're a 9, and I'm the 1 you need.",
-    "If I could rearrange the alphabet I would put U and I together.",
-    "Is your name Google? Because you've got everything I'm searching for."
-];
-
-const smartsies = [
-    "I’ve been wondering, do your lips taste as good as they look.",
-    "If you were a triangle you'd be acute one.",
-    "If beauty were time, you’d be eternity.",
-    "Do you have a tan, or do you always look this hot?",
-    "If you were a fruit you'd be a fineapple.",
-    "I'll give you a kiss. If you don't like it, you can return it.",
-    "Are you from China? Because I'm China get your number.",
-    "Are you craving Pizza? Because I’d love to get a pizz-a you",
-    "Kiss me if I'm wrong, but dinosaurs still exist, right?",
-    "Is your dad a terrorist? Because you are the bomb.",
-    "Are you Australian? Because you meet all of my koala-fications.",
-    "You know what you would really look beautiful in? My arms.",
-    "Are you looking for a stud? Cause I got STD and all I need is U",
-    "My name is Patrick, people call me Pat. But you can call me tonight.",
-    "You must be a magician, because every time I look at you, everyone else disappears."
-];
+const data = {
+    Cheesy: [
+        "Are you French because Eiffel for you.",
+        "I must be a snowflake, because I've fallen for you.",
+        "Hello, I'm a thief, and I'm here to steal your heart.",
+        "Are you cake? Cause I want a piece of that.",
+        "If nothing lasts forever, will you be my nothing?",
+        "Are you from Tennessee? Because you're the only 10 I see!",
+        "What has 36 teeth and holds back the Incredible Hulk? My zipper.",
+        "Are you lost ma'am? Because heaven is a long way from here.",
+        "If you were a steak you would be well done.",
+        "Life without you is like a broken pencil... pointless.",
+        "Are you a 90 degree angle? Cause you are looking right!",
+        "Are you Israeli? Cause you Israeli hot.",
+        "On a scale from 1 to 10, you're a 9, and I'm the 1 you need.",
+        "If I could rearrange the alphabet I would put U and I together.",
+        "Is your name Google? Because you've got everything I'm searching for."
+    ],
+    Dirty: [
+        "Is your name winter? Because you’ll be coming soon.",
+        "I could’ve called heaven and asked for an angel, but I was hoping you’re a slut instead.",
+        "Can you do telekinesis? Because you’ve made a part of me move without even touching it.",
+        "One of my friends told me girls hate oral, do you wanna help me prove him wrong?",
+        "Do you want to commit a sin for your next confessional?",
+        "Your breasts remind me of Mount Rushmore – my face should be among them.",
+        "Your smile is almost as big, warm, and lovely as my penis.",
+        "Are you a raisin? Because you’re raisin my dick.",
+        "Do you run track? Because I heard you Relay want this dick.",
+        "Tell your boobs to stop staring at my eyes.",
+        "Are you a pirate? Because I have a lot of semen waiting for you.",
+        "Are you a shark? Because I’ve got some swimmers for you to swallow.",
+        "Do you believe in karma? Because I know some good karma-sutra positions.",
+        "When I saw you, I lost my tongue. Can I put yours in my mouth?",
+        "I hope you like dragons, because I’ll be dragon my balls across your face tonight.",
+        "You are so selfish. You’re going to have that body the rest of your life and I just want it for one night.",
+        "Let me insert my plug into your socket and we can generate some electricity.",
+        "If I’m a pain in your ass. We can just add more lubricant.",
+        "Are you the lottery lady on TV? Because I’m picturing you holding up my balls.",
+        "I’m gonna have sex with you tonight so you might as well be there.",
+        "Are you a farmer? Because you’ve got some big, round, beautiful melons.",
+        "Did you grow up on a chicken farm? Because you sure know how to raise a cock."
+    ],
+    Smart:  [
+        "I’ve been wondering, do your lips taste as good as they look.",
+        "If you were a triangle you'd be acute one.",
+        "If beauty were time, you’d be eternity.",
+        "Do you have a tan, or do you always look this hot?",
+        "If you were a fruit you'd be a fineapple.",
+        "I'll give you a kiss. If you don't like it, you can return it.",
+        "Are you from China? Because I'm China get your number.",
+        "Are you craving Pizza? Because I’d love to get a pizz-a you",
+        "Kiss me if I'm wrong, but dinosaurs still exist, right?",
+        "Is your dad a terrorist? Because you are the bomb.",
+        "Are you Australian? Because you meet all of my koala-fications.",
+        "You know what you would really look beautiful in? My arms.",
+        "Are you looking for a stud? Cause I got STD and all I need is U",
+        "My name is Patrick, people call me Pat. But you can call me tonight.",
+        "You must be a magician, because every time I look at you, everyone else disappears."
+    ]
+    
+}
--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-
-const cheesyBtn = document.querySelector('.cheesy');
-const dirtyBtn = document.querySelector('.dirty');
-const smartBtn = document.querySelector('.smart');
+const categories = Array.from(document.querySelectorAll('.category'));
 let choice = 'unset';
 
 const displayBox = document.querySelector('.display-box');
@@ -10,19 +7,17 @@ const genBtn = document.querySelector('.generator');
 const textArea = document.querySelector('.text-area');
 const exitBtn = document.querySelector('.exit');
 
-genBtn.addEventListener('click', btn => {
-    switch(choice) {
-        case 'cheesy': 
-        textArea.textContent = cheesies[Math.floor(Math.random() * cheesies.length)];
-        break;
-        case 'dirty':
-        textArea.textContent = dirties[Math.floor(Math.random() * dirties.length)];
-        break;
-        case 'smart':
-        textArea.textContent = smartsies[Math.floor(Math.random() * smartsies.length)];
-        break;
-        default: alert("Select a category first");
-    }
+categories.forEach(function(item){
+    item.addEventListener('click', function() {
+        showBox();
+        textArea.innerHTML = 'Get you some <strong>' + item.value + '</strong> lines';
+        displayTitle.textContent = item.value;
+        choice = item.value;
+
+        genBtn.addEventListener('click', btn => {
+            textArea.textContent = data[item.value][Math.floor(Math.random() * data[item.value].length)];
+        })
+    });
 });
 
 exitBtn.addEventListener('click', btn => {
@@ -34,24 +29,3 @@ function showBox() {
     displayBox.classList.remove('zoomOut');
     displayBox.classList.add('zoomIn');
 }
-
-cheesyBtn.addEventListener('click', btn => {
-    showBox();
-    textArea.textContent = '🧀 Cheesy Hits 🧀';
-    displayTitle.textContent = 'Cheesy';
-    choice = 'cheesy';
-});
-
-dirtyBtn.addEventListener('click', btn => {
-    showBox();
-    textArea.textContent = 'Dirty Hits 👉👌💦';
-    displayTitle.textContent = 'Dirty';
-    choice = 'dirty';
-});
-
-smartBtn.addEventListener('click', function() {
-    showBox();
-    textArea.textContent = '🧠 Smart Hits 🧠';
-    displayTitle.textContent = 'Smart';
-    choice = 'smart';
-});
